d9b38ac66907225084ac18a5853afda09fb3c55a
EuniceTiwa <tsiwaheunice@gmail.com>
2023-07-18 12:13:14 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet - Make the alphabet
- * Return: void
+ *print_alphabet - Make the alphabet
+ *Return: void
  */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
-	{
-	 _putchar(c);
-	}
-         _putchar('\n');
-		 
+	_putchar(c);
+	_putchar('\n');
 }
