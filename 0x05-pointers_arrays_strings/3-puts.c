34ea57f6d0a22b5576d4ac0a73db66729bcc5992
EuniceTiwa <tsiwaheunice@gmail.com>
2023-07-24 11:22:43 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ *
+ * Return: string and new line
+ */
+
+void _puts(char *str)
+{
+	int i = 0
+
+	while (*str 1 = '\0')
+	{
+	_putchar(*str++);
+	}
+	_putchar('\n');
+	return (0);
+}
