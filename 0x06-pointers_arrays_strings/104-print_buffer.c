de51f62228e05f2128c0034afabd29c93fa45a08
EuniceTiwa <tsiwaheunice@gmail.com>
2023-07-27 12:21:42 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,7 +11,6 @@ void print_buffer(char *b, int size)
 	int o, j, i;
 
 	o = 0;
-
 	if (size <= 0)
 	{
 		printf("\n");
@@ -22,16 +21,16 @@ void print_buffer(char *b, int size)
 		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
 		for (i = 0; i < 10; i++)
-	{
+		{
 			if (i < j)
 				printf("%02x", *(b + o + i));
 			else
 				printf("  ");
 			if (i % 2)
 			{
-				printf(" ")
+				printf(" ");
 			}
-	}
+		}
 		for (i = 0; i < j; i++)
 		{
 			int c = *(b + o + i);
@@ -44,5 +43,5 @@ void print_buffer(char *b, int size)
 		}
 		printf("\n");
 		o + = 10;
-	}
+		}
 }
